20131de1709ae93fc44a29318e3e12fbbd209af9
Jader <jvjader@gmail.com>
2022-03-15 12:46:06 +0100

    successfully unfucked

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -49,6 +49,8 @@ extern "C" {
 
 /* USER CODE END EM */
 
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+
 /* Exported functions prototypes ---------------------------------------------*/
 void Error_Handler(void);
 
@@ -65,6 +67,10 @@ void Error_Handler(void);
 #define USART_RX_GPIO_Port GPIOA
 #define LD2_Pin GPIO_PIN_5
 #define LD2_GPIO_Port GPIOA
+#define Button_down_Pin GPIO_PIN_8
+#define Button_down_GPIO_Port GPIOA
+#define Button_up_Pin GPIO_PIN_9
+#define Button_up_GPIO_Port GPIOA
 #define TMS_Pin GPIO_PIN_13
 #define TMS_GPIO_Port GPIOA
 #define TCK_Pin GPIO_PIN_14
---
+++ Core/Src/GameEngine.cpp
@@ -0,0 +1,28 @@
+/*
+ * GameEngine.cpp
+ *
+ *  Created on: Mar 9, 2022
+ *      Author: jvjad
+ */
+
+#include "GameEngine.h"
+
+void gameInit(){
+	GameEngine engine();
+
+	engine().run();
+}
+
+GameEngine::GameEngine() {
+	// TODO Auto-generated constructor stub
+
+}
+
+GameEngine::~GameEngine() {
+	// TODO Auto-generated destructor stub
+}
+
+void GameEngine::run()
+{
+
+}
